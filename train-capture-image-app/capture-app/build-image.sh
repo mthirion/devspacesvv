4da65f9b403541310ebc874e0b9b0d9130657c91
Michael Thirion <mthirion@mthirion-thinkpadp1gen5.remote.csb>
2024-07-02 10:25:18 +0200

    test devspace number 3

--- train-capture-image-app/capture-app/build-image.sh
+++
@@ -1,4 +0,0 @@
-mvn clean package
-podman build -f src/main/docker/Dockerfile.opencv -t quarkus/train-capture-app-jvm --platform linux/arm64/v8 . 
-podman tag quarkus/train-capture-app-jvm:latest quay.io/mouachan/train-capture-app-arm64:latest    
-podman push quay.io/mouachan/train-capture-app-arm64:latest 
